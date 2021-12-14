b307d9fb3f47b0907711667b2927bef7621e28ad
Jonathan Karon <jkaron@newrelic.com>
2021-12-14 08:41:38 -0800

    feat: include nr agent version in csv output

--- nr-find-log4j.js
+++ nr-find-log4j.js
@@ -472,7 +472,7 @@ function writeResults(state) {
     }
 
     if (useCsv) {
-        const columns = ['accountId', 'applicationId', 'name', 'log4jJar', 'log4jJarVersion', 'log4jJarSha1', 'log4jJarSha512', 'nrUrl'];
+        const columns = ['accountId', 'applicationId', 'name', 'agentVersion', 'log4jJar', 'log4jJarVersion', 'log4jJarSha1', 'log4jJarSha512', 'nrUrl'];
         const outputFile = `log4j_scan_${state.region}_${fileTimestamp}.csv`;
         // DIY rather than depend on a csv module
         fs.writeFileSync(
